80317f1be30dee70e7abc4f9ac7a1fd429b1a734
alto <altostratous@gmail.com>
2017-01-07 01:58:15 +0330

    Added upload system.

--- dockerfiles/models.py
+++ dockerfiles/models.py
@@ -1,6 +1,11 @@
 from django.db import models
+import subprocess
 
 
 # Create your models here.
 class DockerFile(models.Model):
-    repository = models.TextField()
+    repository = models.TextField(unique=True)
+    file = models.FileField(upload_to='dockerfiles')
+
+    def save(self, force_insert=False, force_update=False, using=None, update_fields=None):
+        super().save(force_insert, force_update, using, update_fields)
